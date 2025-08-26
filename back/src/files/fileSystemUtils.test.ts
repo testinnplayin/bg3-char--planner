33718b7fd6d5bfe2c1ed78bef5cadcd060f963fe
Rachelle Wood <rachelle.wood.ext@allianz-trade.com>
2025-08-26 12:13:46 +0200

    Start adding domain

--- back/src/files/fileSystemUtils.test.ts
+++ back/src/files/fileSystemUtils.test.ts
@@ -4,12 +4,16 @@ import * as fileSystemUtils from './fileSystemUtils';
 
 describe('fileSystemUtils', () => {
   describe('readDataFile', () => {
-    test.only('it returns a string from a JSON file', async (t) => {
+    test('it returns a string from a JSON file', async (t) => {
       const testFilePath = '../../tests/files/test.json';
       const expected = '{\n  "foo": "bar"\n}';
       const result = await fileSystemUtils.readDataFile(testFilePath);
       t.diagnostic(result);
       assert.strictEqual(result, expected);
+    });
+
+    test('todo() it should throw an exception if no file', (t) => {
+      t.todo();
     });
   });
 });
